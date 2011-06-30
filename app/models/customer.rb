6b0c538463745da5415a7fea838231fac1d2e1d6
Evan Howlett <evmeisterx@gmail.com>
2011-06-30 17:01:15 -0500

    modified:   .idea/workspace.xml 	modified:   app/models/customer.rb 	modified:   app/views/customers/_form.html.erb 	modified:   app/views/customers/index.html.erb 	modified:   app/views/customers/show.html.erb

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -11,7 +11,8 @@ class Customer < ActiveRecord::Base
   has_one :stepeight
   has_one :stepnine
   has_one :stepten
-
+  STAGES = ['0. Customer Info', '1. Structure and Submit', '2. Order Appraisal', '3. More Steps Go Here']
+  SALESMEN = ['Purvis', 'Hildreth', 'Turner', 'Conklin']
     def self.search(search)
     if search
       where('lname LIKE ?', "%#{search}%")
